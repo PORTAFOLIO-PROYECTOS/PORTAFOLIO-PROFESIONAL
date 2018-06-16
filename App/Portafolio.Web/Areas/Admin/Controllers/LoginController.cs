d49564cc0d83f0d3fc966424520583edf4d6b406
Hugo <hugo.rock20@hotmail.com>
2018-06-15 23:19:52 -0500

    class filters AutenticadoAttribute

--- App/Portafolio.Web/Areas/Admin/Controllers/LoginController.cs
+++ App/Portafolio.Web/Areas/Admin/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Helper;
 using Portafolio.Model;
+using Portafolio.Web.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Web.Mvc;
 
 namespace Portafolio.Web.Areas.Admin.Controllers
 {
+    [NoLogin]
     public class LoginController : Controller
     {
         private Usuario usuario = new Usuario();
