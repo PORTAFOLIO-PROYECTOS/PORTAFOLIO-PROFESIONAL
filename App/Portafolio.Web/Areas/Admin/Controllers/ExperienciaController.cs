d53ec7dbe6f3e58c39ce3f42f591d9e62b10d012
Hugo <hugo.rock20@hotmail.com>
2018-06-17 16:20:04 -0500

    maintenance of experience and studies completed

--- App/Portafolio.Web/Areas/Admin/Controllers/ExperienciaController.cs
+++ App/Portafolio.Web/Areas/Admin/Controllers/ExperienciaController.cs
@@ -1,4 +1,7 @@
-﻿using Portafolio.Web.Areas.Admin.Filters;
+﻿using Helper;
+using Model;
+using Portafolio.Model;
+using Portafolio.Web.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +13,52 @@ namespace Portafolio.Web.Areas.Admin.Controllers
     [Autenticado]
     public class ExperienciaController : Controller
     {
+        private Experiencia experiencia = new Experiencia();
+
         // GET: Admin/Experiencia
-        public ActionResult Index(int tipo)
+        public ActionResult Index(int tipo = 1)
         {
+            ViewBag.Tipo = tipo;
             ViewBag.Title = tipo == 1 ? "Trabajos realizados" : "Estudios previos";
             return View();
+        }
+
+        public JsonResult Listar(AnexGRID grid, int tipo)
+        {
+            return Json(experiencia.Listar(grid, tipo));
+        }
+
+        public ActionResult Crud(byte tipo = 0, int id = 0)
+        {
+            if (id == 0)
+            {
+                if (tipo == 0) return Redirect("~/Admin/Experiencia");
+                experiencia.Tipo = tipo;
+                experiencia.Usuario_id = SessionHelper.GetUser();
+            }
+            else experiencia = experiencia.Obtener(id);
+
+            return View(experiencia);
+        }
+
+        public JsonResult Guardar(Experiencia model)
+        {
+            var rm = new ResponseModel();
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if (rm.response) rm.href = Url.Content("~/Admin/Experiencia?tipo=" + model.Tipo);
+            }
+            return Json(rm);
+        }
+
+        public JsonResult Eliminar(int id)
+        {
+            var rm = experiencia.Eliminar(id);
+
+            if (rm.response) rm.href = "self"; // sabe que tiene que refrescar la pagina
+
+            return Json(rm);
         }
     }
 }
