c6890643075d6b838fcfb5d4a71ff6b4b0e91ed4
Hugo <hugo.rock20@hotmail.com>
2018-06-21 23:27:04 -0500

    form contact finished

--- App/Portafolio.Web/Controllers/DefaultController.cs
+++ App/Portafolio.Web/Controllers/DefaultController.cs
@@ -1,8 +1,11 @@
-﻿using Portafolio.Model;
+﻿using Model;
+using Portafolio.Model;
 using Portafolio.Web.App_Start;
+using Portafolio.Web.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +19,42 @@ namespace Portafolio.Web.Controllers
         public ActionResult Index()
         {
             return View(usuario.Obtener(FrontOfficeStartUp.UsuarioVisualizando(), true));
+        }
+
+        public JsonResult EnviarCorreo(ContactoViewModel model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var _usuario = usuario.Obtener(FrontOfficeStartUp.UsuarioVisualizando());
+                    var mail = new MailMessage();
+                    mail.From = new MailAddress(model.Correo, model.Nombre);
+                    mail.To.Add(_usuario.Email);
+                    mail.Subject = "Correo desde contato";
+                    mail.IsBodyHtml = true;
+                    mail.Body = model.Mensaje;
+
+                    var smtserver = new SmtpClient("smtp.live.com");
+                    smtserver.Port = 587;
+                    smtserver.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtserver.UseDefaultCredentials = false;
+                    smtserver.Credentials = new System.Net.NetworkCredential("", "");
+                    smtserver.EnableSsl = true;
+                    smtserver.Send(mail);
+                }
+                catch (Exception e)
+                {
+                    rm.SetResponse(false, e.Message);
+                    return Json(rm);
+                    throw;
+                }
+                rm.SetResponse(true);
+                rm.function = "CerrarContacto();";
+            }
+            return Json(rm);
         }
     }
 }
